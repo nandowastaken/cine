f5fd84327cbea6a402589fcae46802fce7c6c98f
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-01-30 14:12:35 -0300

    add aboutus page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
+import AboutUs from "./pages/AboutUs";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <AboutUs />
     </div>
   );
 }
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,44 @@
+import "../styles/AboutUs.css";
+
+export default function AboutUs() {
+  return (
+    <div className="AboutUs">
+      <div className="whoWeAre">
+        <h1 className="aboutUsSectionTitle">Quem somos?</h1>
+        <p className="aboutUsSectionDescription">
+          Nós somos o Cineclube, Cine pros mais íntimos, somos um clube de
+          cinema do IFAL campus Maceió que visa reunir alunos que gostam muito
+          de filmes para discutir, formar novas amizades, e aprender mais sobre
+          o cinema. Tudo começou em 2021, no meio da pandemia, quando a cinéfila
+          Talita Raquel criou este clube, e nós seguimos adiante.
+        </p>
+      </div>
+
+      <div className="howItWorks">
+        <h1 className="aboutUsSectionTitle">Como funciona?</h1>
+
+        <div className="howItWorksDescContainer">
+          <img src="../src/assets/left-arrow.svg" alt="" className="arrow" />
+          <p className="aboutUsSectionDescription">
+            Todo mês nós temos duas reuniões em uma Sexta-feira, uma no turno da
+            manhã e a outra na da tarde, sobre um filme escolhido previamente. O
+            tema e a data serão sempre exibidos neste site, então não precisam
+            ficar preocupados em ficarem desinformados.
+          </p>
+          <img src="../src/assets/right-arrow.svg" alt="" className="arrow" />
+        </div>
+      </div>
+
+      <div className="nextMeeting">
+        <h1 className="aboutUsSectionTitle">Próxima Reunião</h1>
+        <p className="aboutUsSectionDescription">
+          Nossa próxima reunião é Sexta-feira, 25/11, sobre o filme Psicose
+          (1960), do Alfred Hitchcock, nos horários da manhã de 11h até 12h, e
+          de tarde, de 13h até 14h.
+        </p>
+
+        <button className="seeMovie">Ver Filme</button>
+      </div>
+    </div>
+  );
+}
