5f8f62fd2a7e2a327b28003ef586bc47163f8198
Ethan <ysa4@aluno.ifal.edu.br>
2023-02-17 17:13:26 -0300

    adicionando react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-import LandingPage from "./pages/LandingPage";
-import AboutUs from "./pages/AboutUs";
-import MovieSection from "./pages/MovieSection";
-import Login from "./pages/Login";
-
-import HomeOscar from "./pages/oscar/HomeOscar";
+import LandingPage from './pages/LandingPage';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <HomeOscar />
+      <LandingPage />
+      <AboutUs />
     </div>
   );
 }
