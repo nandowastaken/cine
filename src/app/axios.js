5bfac97276606e0620b4f9bf0ac4d227916fa9c8
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-03-04 19:19:27 -0300

    trying to solve vercel error

--- src/app/axios.js
+++ src/app/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const cors = require("cors");
+app.use(cors());
+
 const instance = axios.create({
-  baseURL: 'https://cineclube.onrender.com',
-  withCredentials: true
+  baseURL: "https://cineclube.onrender.com",
+  withCredentials: true,
 });
 
 export default instance;
